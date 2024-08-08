94c9076728492cdd93eb00284440524f96bc05e3
TJ Zhang <tj.zhang@improving.com>
2024-08-07 17:31:15 -0700

    Node: Add command GETEX
    Signed-off-by: TJ Zhang <tj.zhang@improving.com>

--- node/src/BaseClient.ts
+++ node/src/BaseClient.ts
@@ -80,6 +80,7 @@ import {
     createGet,
     createGetBit,
     createGetDel,
+    createGetEx,
     createGetRange,
     createHDel,
     createHExists,
@@ -198,6 +199,7 @@ import {
     createZScore,
     StreamClaimOptions,
     createXClaim,
+    GetExOptions,
 } from "./Commands";
 import {
     ClosingError,
@@ -839,6 +841,24 @@ export class BaseClient {
      */
     public get(key: string): Promise<string | null> {
         return this.createWritePromise(createGet(key));
+    }
+
+    /**
+     * Get the value of `key` and optionally set its expiration. `GETEX` is similar to {@link get}
+     * See https://valkey.io/commands/getex for more details.
+     *
+     * @param key - The key to retrieve from the database.
+     * @param options - (Optional) set expiriation to the given key.
+     * @returns If `key` exists, returns the value of `key` as a `string`. Otherwise, return `null`.
+     *
+     * @example
+     * ```typescript
+     * const result = await client.getex("key", {expiry: { type: "seconds", count: 5 }});
+     * console.log(result); // Output: 'value'
+     * ```
+     */
+    public getex(key: string, options?: GetExOptions): Promise<string | null> {
+        return this.createWritePromise(createGetEx(key, options));
     }
 
     /**
